cd3a44f717e403e4e0924d92f2e90867fde9df37
roy <zzl11081@gmail.com>
2015-04-04 21:25:28 +0800

    增加浏览器兼容性

---
+++ test/data/effectExplain.js
@@ -0,0 +1,236 @@
+var effect_list = {
+    "bounce" : {
+       "compatibility" : null    
+    },
+    "bounceIn" : {
+       "compatibility" : null    
+    },
+    "bounceInDown" : {
+       "compatibility" : null    
+    },
+    "bounceInLeft" : {
+       "compatibility" : null    
+    },
+    "bounceInRight" : {
+       "compatibility" : null    
+    },
+    "bounceInUp" : {
+       "compatibility" : null    
+    },
+    "bounceOut" : {
+       "compatibility" : null    
+    },
+    "bounceOutDown" : {
+       "compatibility" : null    
+    },
+    "bounceOutLeft" : {
+       "compatibility" : null    
+    },
+    "bounceOutRight" : {
+       "compatibility" : null    
+    },
+    "bounceOutUp" : {
+       "compatibility" : null    
+    },
+    "circular" : {
+       "compatibility" : { "ie":"No", "opera":"No" }    
+    },
+    "circularAnti" : {
+       "compatibility" : { "ie":"No", "opera":"No" }    
+    },
+    "circularEaseIn" : {
+       "compatibility" : { "ie":"No", "opera":"No" }    
+    },
+    "circularEaseOut" : {
+       "compatibility" : { "ie":"No", "opera":"No" }    
+    },
+    "fadeIn" : {
+       "compatibility" : null    
+    },
+    "fadeInDown" : {
+       "compatibility" : null    
+    },
+    "fadeInDownBig" : {
+       "compatibility" : null    
+    },
+    "fadeInLeft" : {
+       "compatibility" : null    
+    },
+    "fadeInLeftBig" : {
+       "compatibility" : null    
+    },
+    "fadeInRight" : {
+       "compatibility" : null    
+    },
+    "fadeInRightBig" : {
+       "compatibility" : null    
+    },
+    "fadeInUp" : {
+       "compatibility" : null    
+    },
+    "fadeInUpBig" : {
+       "compatibility" : null    
+    },
+    "fadeOut" : {
+       "compatibility" : null    
+    },
+    "fadeOutDown" : {
+       "compatibility" : null    
+    },
+    "fadeOutDownBig" : {
+       "compatibility" : null    
+    },
+    "fadeOutLeft" : {
+       "compatibility" : null    
+    },
+    "fadeOutLeftBig" : {
+       "compatibility" : null    
+    },
+    "fadeOutRight" : {
+       "compatibility" : null    
+    },
+    "fadeOutRightBig" : {
+       "compatibility" : null    
+    },
+    "fadeOutUp" : {
+       "compatibility" : null    
+    },
+    "fadeOutUpBig" : {
+       "compatibility" : null    
+    },
+    "flip" : {
+       "compatibility" : null    
+    },
+    "flipInX" : {
+       "compatibility" : null    
+    },
+    "flipInY" : {
+       "compatibility" : null    
+    },
+    "flipOutX" : {
+       "compatibility" : null    
+    },
+    "flipOutY" : {
+       "compatibility" : null    
+    },
+    "lightSpeedIn" : {
+       "compatibility" : null    
+    },
+    "lightSpeedOut" : {
+       "compatibility" : null    
+    },
+    "rotateIn" : {
+       "compatibility" : null    
+    },
+    "rotateInDownLeft" : {
+       "compatibility" : null    
+    },
+    "rotateInDownRight" : {
+       "compatibility" : null    
+    },
+    "rotateInUpLeft" : {
+       "compatibility" : null    
+    },
+    "rotateInUpRight" : {
+       "compatibility" : null    
+    },
+    "rotateOut" : {
+       "compatibility" : null    
+    },
+    "rotateOutDownLeft" : {
+       "compatibility" : null    
+    },
+    "rotateOutDownRight" : {
+       "compatibility" : null    
+    },
+    "rotateOutUpLeft" : {
+       "compatibility" : null    
+    },
+    "rotateOutUpRight" : {
+       "compatibility" : null    
+    },
+    "flash" : {
+       "compatibility" : null    
+    },
+    "pulse" : {
+       "compatibility" : null    
+    },
+    "rubberBand" : {
+       "compatibility" : null    
+    },
+    "shake" : {
+       "compatibility" : null    
+    },
+    "swing" : {
+       "compatibility" : null    
+    },
+    "tada" : {
+       "compatibility" : null    
+    },
+    "wobble" : {
+       "compatibility" : null    
+    },
+    "slideInDown" : {
+       "compatibility" : null    
+    },
+    "slideInLeft" : {
+       "compatibility" : null    
+    },
+    "slideInRight" : {
+       "compatibility" : null    
+    },
+    "slideInUp" : {
+       "compatibility" : null    
+    },
+    "slideOutDown" : {
+       "compatibility" : null    
+    },
+    "slideOutLeft" : {
+       "compatibility" : null    
+    },
+    "slideOutRight" : {
+       "compatibility" : null    
+    },
+    "slideOutUp" : {
+       "compatibility" : null    
+    },
+    "hinge" : {
+       "compatibility" : null    
+    },
+    "rollIn" : {
+       "compatibility" : null    
+    },
+    "rollOut" : {
+       "compatibility" : null    
+    },
+    "zoomIn" : {
+       "compatibility" : null    
+    },
+    "zoomInDown" : {
+       "compatibility" : null    
+    },
+    "zoomInLeft" : {
+       "compatibility" : null    
+    },
+    "zoomInRight" : {
+       "compatibility" : null    
+    },
+    "zoomInUp" : {
+       "compatibility" : null    
+    },
+    "zoomOut" : {
+       "compatibility" : null    
+    },
+    "zoomOutDown" : {
+       "compatibility" : null    
+    },
+    "zoomOutLeft" : {
+       "compatibility" : null    
+    },
+    "zoomOutRight" : {
+       "compatibility" : null    
+    },
+    "zoomOutUp" : {
+       "compatibility" : null    
+    }
+};
