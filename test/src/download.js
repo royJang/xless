554e1f390738123b354501e8d12a6c6b37ba095b
roy <zzl11081@gmail.com>
2015-04-11 19:09:27 +0800

    增加css下载

--- test/src/download.js
+++ test/src/download.js
@@ -4,20 +4,21 @@
     var downloadLock = false;
     var downloadMsg = $(".download-msg");
     var download_less_btn = $(".download_less");
+    var download_css_btn = $(".download_css");
     var animateList = $(".download-channel-animate-list"),
         componentsList = $(".download-channel-components-list");
 
-    download_less_btn.on("click", function (){
-        downloadMsg.html("开始分析文件...");
-        if( downloadLock ){
-            downloadMsg.html("正在分析下载...");
-            return;
-        }
-        downloadLock = true;
 
+    var getDownloadFileList = function ( callback ){
         //所需要下载的文件列表
-        var downloadArr = ['src/basic/mixins/animation.less',
-            'src/animation/base.less'];
+        var $$file = [
+            'src/basic/mixins/animation.less',
+            'src/basic/mixins/transform.less',
+            'src/basic/mixins/transition.less',
+            'src/animation/base.less'
+        ];
+        var downloadArr = [].concat($$file);
+        var $fileList = [];
 
         animateList.find("li").each(function (i, el){
             var $el = $(el),
@@ -34,6 +35,8 @@
                 downloadArr.push(
                     "src/animation/" + dirName + "/" + t + ".less"
                 );
+
+                $fileList.push(t);
             }
         });
 
@@ -49,25 +52,67 @@
         downloadArr.forEach(function ( el ){
             ajaxTasks.push(function (callback){
                 $.get(el, function (data){
-                    callback(null,data);
+                    callback(null, data);
                 })
             })
         });
 
         async.series(ajaxTasks, function (err,result){
-            var blob = new Blob(result, { type : 'text/less' });
-            var link = window.URL.createObjectURL(blob);
-            var atag = document.createElement("a");
-            atag.href = link;
-            atag.download = "xless.less";
-            document.body.appendChild(atag);
-            atag.click();
-            downloadMsg.html("下载完毕...");
-            setTimeout(function (){
-                downloadLock = false;
-                downloadMsg.html("");
-                window.URL.revokeObjectURL(blob);
-            },1000)
+            return callback && callback(err,{
+                    data : result,
+                    fileList : $fileList
+                });
         });
+    };
+
+    var createDownloadLink = function ( result, type, fileName ){
+        downloadMsg.html("开始分析文件...");
+        if( downloadLock ){
+            downloadMsg.html("正在分析下载...");
+            return;
+        }
+        downloadLock = true;
+        var blob = new Blob( result, { type : type });
+        var link = window.URL.createObjectURL(blob);
+        var atag = document.createElement("a");
+        atag.href = link;
+        atag.download = fileName;
+        document.body.appendChild(atag);
+        atag.click();
+        downloadMsg.html("下载完毕...");
+        setTimeout(function (){
+            downloadLock = false;
+            downloadMsg.html("");
+            window.URL.revokeObjectURL(blob);
+        },1000)
+    };
+
+    //less文件下载
+    download_less_btn.on("click", function (){
+        getDownloadFileList(function (err,result){
+            createDownloadLink(result.data, "text/css", "xless.less");
+        })
     });
+
+    //css文件下载
+    download_css_btn.on("click", function (){
+        getDownloadFileList(function (err,result){
+            var $str = "";
+
+            //链接所有less内容
+            result.data.forEach(function (el){
+                $str += el; 
+            });
+
+            //添加对less的执行，这样才可以编译出来
+            result.fileList.forEach(function (el){
+                $str += ( "\n.xless-" + el + "{ ." + el + "(); }");
+            });
+
+            //编译less文件
+            less.render($str, function (e, output) {
+                createDownloadLink([output.css], "text/css", "xless.css");
+            });
+        });
+    })
 })();
